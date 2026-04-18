13b90e1691d7da51ffa18e50c80e9791f63f7993
danyhd24 <20232389@uatx.mx>
2026-04-17 19:19:02 -0600

    Modulo 7
    Se le añadió mensajes emergentes para aceptar o rechazar a los alumnos
    Se actualizaron los archivos para que coincidan con la última version de la base de datos 
    Se agregaron apellidos materno y paterno

--- app/CU_07_AdministrarAlumnos/aceptar_alumnos.js
+++ app/CU_07_AdministrarAlumnos/aceptar_alumnos.js
@@ -13,11 +13,30 @@ function aceptarAlumno(matricula){
     })
     .then(function(datos){
         if(datos.success){
-            alert("Alumno aceptado");
+            lanzarToast("¡Alumno aceptado correctamente!    ", "exito");
             cargarAlumnos();
         }
     })
     .catch(function(error){
         console.error("Error", error);
     })
+}
+
+function lanzarToast(texto, tipo) {
+    const toast = document.getElementById('toast-mensaje');
+    
+    // 1. Limpiamos clases previas y ponemos la nueva
+    toast.className = 'toast'; // Resetea a la base
+    toast.classList.add(tipo); // Agrega 'exito' o 'error'
+    
+    // 2. Insertamos el texto
+    toast.innerText = texto;
+    
+    // 3. Mostramos
+    toast.classList.remove('oculto');
+
+    // 4. Desvanecemos en 3 segundos
+    setTimeout(() => {
+        toast.classList.add('oculto');
+    }, 3000);
 }
