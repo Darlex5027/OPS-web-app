96ca97cdb1e6f966a8689ce1a75dd31b8cd86a7f
danyhd24 <20232389@uatx.mx>
2026-04-22 18:16:56 -0600

    Modulo 7
    Uso de cookies para filtrar los alumnos

--- app/CU_07_AdministrarAlumnos/obtener_alumnos_pendientes.php
+++ app/CU_07_AdministrarAlumnos/obtener_alumnos_pendientes.php
@@ -3,6 +3,7 @@ require_once '../php/db.php';
 
 try{
     $pdo = new PDO($dsn, $user, $pass, $options);
+    $id_carrera = $_COOKIE['Id_carrera'];
     $consulta = $pdo->prepare("SELECT 
     CONCAT (Alumnos.Nombre, ' ', Alumnos.Apellido_M, ' ', Alumnos.Apellido_P) As Nombre_Completo, 
     Carreras.Nombre as Nombre_Carrera, 
@@ -13,8 +14,8 @@ try{
     WHERE Actividades_Alumnos.Id_servicio=Actividades.Id_servicio 
     AND Carreras.Id_carrera=Alumnos.Id_carrera AND Usuarios.Id_tipo_usuario=2 
     AND Usuarios.Id_usuario=Alumnos.Id_usuario AND Alumnos.Id_alumno=Actividades_Alumnos.Id_alumno 
-    AND Usuarios.Activo=0");
-    $consulta->execute();   
+    AND Usuarios.Activo=0 AND Carreras.Id_carrera=?");
+    $consulta->execute([$id_carrera]);   
     echo json_encode($consulta->fetchAll());
 } catch (\PDOException $e){
     http_response_code(500);
