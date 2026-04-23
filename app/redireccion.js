70ce7735d3ec979c0304861f42414842a38f8bb7
Darlex5027 <reseal50@gmail.com>
2026-04-23 10:37:01 -0600

    feat/Verificación de sesión

--- app/redireccion.js
+++ app/redireccion.js
@@ -1,7 +1,23 @@
+/*
+ * Archivo     : redireccion.js
+ * Módulo      : Para todos los modulos, verificación de sesión.
+ * Autor       : Alejandro Resendiz Reyes
+ * Fecha       : 23/04/2026
+ * Descripción : Verifica la sesión del usuario mediante fetch a verificar_sesion.php.
+ *               Si la sesión no es válida, redirige al login automáticamente.
+ */
+
 document.addEventListener('DOMContentLoaded', redireccionar);
 
 function redireccionar(){
-	if(!document.cookie){
-		window.location.href = "../CU_01_Login/login.html";
-	}
+	fetch('./verificar_sesion.php')
+	.then(res => res.json())
+	.then(data => {
+		if (data.error){
+			window.location.href = '../CU_01_Login/login.html';
+		}
+	})
+	.cat(() => {
+		window.location.href = '../CU_01_Login/login.html';
+	});
 }
