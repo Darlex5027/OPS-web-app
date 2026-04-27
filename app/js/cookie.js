b0f2200d268517839ba2e1e111b054e009352f79
danyhd24 <20232389@uatx.mx>
2026-04-27 14:45:21 -0600

    docs / comentarios de bloque descriptivo obtenerCookie()
    Comentarios Nombre del autor, fecha de creación y breve descripción de tu funcionalidad del archivo 
    Se modifico el documento cookies.js de la carpeta js, se añadieron comentarios al inicio del archivo para tener control de sus datos de creación

--- app/js/cookie.js
+++ app/js/cookie.js
@@ -1,3 +1,9 @@
+/* 
+Daniela Hernandez Hernandez
+Fecha de creacion: 23 de abril del 2026
+El archivo cookie.js es una utilidad diseñada para extraer el valor de una cookie 
+específica almacenada en el navegador mediante los datos enviados el login.js
+*/
 export { obtenerCookie }
 
 const obtenerCookie = (name) => {
