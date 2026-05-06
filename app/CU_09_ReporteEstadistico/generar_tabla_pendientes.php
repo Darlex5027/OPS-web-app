9e23dc18afc22d16cbf18db79a752c086f05c856
Darlex5027 <reseal50@gmail.com>
2026-05-06 01:41:36 -0600

    Listado de alumnos con cuestionario completado y pendientes - Ya se pueden listar los alumnos que han contestado el formulario seleccionado, filtrados por carrera, y periodo - Ya se pueden listar los alumnos que no han contestado el formulario seleccionado, filtrados por carrera y periodo - Se editaron y agregarón demasiados archivos.

---
+++ app/CU_09_ReporteEstadistico/generar_tabla_pendientes.php
@@ -0,0 +1,119 @@
+<?php
+/*
+  Archivo     : generar_tabla_pendientes.php
+  Módulo      : CU_09_ReporteEstadistico
+  Autor       : Alejandro Resendiz Reyes
+  Fecha       : 23/04/2026		
+  Descripción : Este archivo se encarga de generar la tabla de resultados para el reporte estadístico.
+				Se conecta a la base de datos utilizando PDO, recibe los parámetros de filtrado
+				en formato JSON, ejecuta una consulta para obtener los resultados agrupados por pregunta,
+				y devuelve los resultados en formato JSON. Si ocurre algún error durante la conexión,
+				el procesamiento de los datos o la consulta, se devuelve un mensaje de error adecuado.
+
+ */
+require_once '../php/db.php';
+
+
+// Se intenta establecer una conexión a la base de datos utilizando PDO. Si ocurre un error, 
+// se devuelve un mensaje de error y se detiene la ejecución.
+try {
+	$pdo = new PDO($dsn, $user, $pass, $options);
+} catch (Exception $error_pdo) {
+	http_response_code(500);
+	echo json_encode(['error' => 'Hubo un error creando el PDO']);
+	exit();
+}
+
+// Se intenta decodificar los datos recibidos en formato JSON para obtener
+// los filtros seleccionados. Si ocurre un error, 
+// se devuelve un mensaje de error y se detiene la ejecución.
+try {
+	// Se decodifican los datos recibidos en formato JSON para obtener los filtros seleccionados
+	$valores = json_decode(file_get_contents("php://input"), true);
+	// Se asignan los valores de los filtros a variables para su uso posterior
+	$id_encuesta   = $valores['Id_encuesta'];
+	$id_carrera    = $_COOKIE["Id_carrera"];
+	$periodo_tipo  = $valores['Periodo_tipo'];
+	$periodo_anio  = $valores['Periodo_anio'];  // Renombrada para evitar tilde
+} catch (Exception $error_json) {
+	// Si ocurre un error al decodificar los datos JSON, se devuelve un mensaje de error y se detiene la ejecución
+	http_response_code(500);
+	echo json_encode(['error' => 'Hubo un error pasando los datos de filtrado.']);
+	exit();
+}
+
+
+try {
+	$query = "SELECT Encuestas.Id_servicio FROM Encuestas WHERE Encuestas.Id_Encuesta=?";
+	$stmt = $pdo->prepare($query);
+
+	// Se ejecuta la consulta SQL utilizando los filtros seleccionados como parámetros
+	$stmt->execute([$id_encuesta]);
+
+	// Se obtiene el resultado de la consulta en formato de arreglo asociativo
+	$id_servicio_encuesta = $stmt->fetchColumn();
+
+
+}catch (Exception $error_json){
+	http_response_code(500);
+	echo json_encode(['error' => 'Hubo un error obteniendo datos sobre la encuesta']);
+	exit();
+}
+
+// Se intenta ejecutar la consulta para obtener los resultados agrupados por pregunta según los filtros seleccionados.
+try {
+	// Se define la consulta SQL para obtener los resultados agrupados por pregunta según los filtros seleccionados
+	$query = "
+SELECT DISTINCT
+    CONCAT(
+        AL.Nombre,
+        ' ',
+        AL.Apellido_P,
+        ' ',
+        AL.Apellido_M
+    ) AS 'Nombre del Alumno',
+    E.Nombre AS 'Nombre encuesta',
+    CONCAT(
+        PE.Periodo_tipo,
+        ' ',
+        PE.Periodo_año
+    ) AS 'Periodo',
+    A.Servicio AS 'Servicio'
+FROM
+    Encuestas E
+JOIN Periodo_Encuesta PE ON
+    E.Id_encuesta = PE.Id_encuesta
+JOIN Actividades_Alumnos AA ON
+    AA.periodo_tipo = PE.Periodo_tipo AND AA.periodo_año = PE.Periodo_año AND AA.Id_servicio = E.Id_servicio
+JOIN Actividades A ON
+    A.Id_servicio = AA.Id_servicio
+JOIN Alumnos AL ON
+    AL.Id_alumno = AA.Id_alumno
+LEFT JOIN Respuestas R ON
+    R.Id_encuesta = E.Id_encuesta AND R.Id_alumno = AL.Id_alumno
+WHERE
+    R.Id_encuesta IS NULL AND E.Id_encuesta = ? AND AL.Id_carrera = ? AND AA.Id_servicio = ? AND PE.Periodo_tipo = ? AND PE.Periodo_año = ?
+";
+	// Se prepara y ejecuta la consulta SQL utilizando los filtros seleccionados como parámetros
+	$stmt = $pdo->prepare($query);
+
+	// Se ejecuta la consulta SQL utilizando los filtros seleccionados como parámetros
+	$stmt->execute([$id_encuesta, $id_carrera, $id_servicio_encuesta, $periodo_tipo, $periodo_anio]);
+
+	// Se obtiene el resultado de la consulta en formato de arreglo asociativo
+	$tabla_resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+	if (empty($tabla_resultado)) {                       
+		// Si no se obtienen resultados para la consulta, se devuelve un mensaje de error indicando que no hay resultados disponibles
+		http_response_code(404);
+		echo json_encode(['error' => 'No hay resultados para disponibles.']);
+	} else {
+		// Si se obtienen resultados para la consulta, se devuelve el resultado en formato JSON
+		echo json_encode($tabla_resultado);
+	}
+
+} catch (Exception $error_consulta) {
+	// Si ocurre un error al ejecutar la consulta, se devuelve un mensaje de error con el detalle del error
+	http_response_code(500);
+	echo json_encode(['error' => $error_consulta->getMessage()]);
+}
--- app/CU_09_ReporteEstadistico/reporte_estadistico.js
+++ app/CU_09_ReporteEstadistico/reporte_estadistico.js
@@ -3,35 +3,19 @@
   Módulo      : CU_09_ReporteEstadistico
   Autor       : Alejandro Resendiz Reyes
   Fecha       : 23/04/2026		
-  Descripción : Archivo JS para el reporte estadístico,
-  				se encarga de cargar los datos de las
-				encuestas, periodos y generar la tabla
-				con los resultados obtenidos del php, 
-				ademas de generar los Excel y PDF sobre 
-				la tabla generada.
+  Descripción : Archivo JS para el reporte estadístico se encarga de renderizar el menu y cargar las encuestas en el select.
 
 */
-import { obtenerCookie } from '../js/cookie.js';
-import { lanzarToast } from '../js/lanzar_toast.js';
 import { renderMenu } from '../js/menu.js';
 
-// Al final del archivo
-window.handleCargarDatos = handleCargarDatos;
-
 const elEncuestas = document.getElementById('slctEncuestas');
-const elPeriodo_tipo = document.getElementById('slctPeriodoTipo');
-const elPeriodo_anio= document.getElementById('slctPeriodoAnio');
-
-//Al iniciar la página, se ocultan los filtros de periodo y año hasta que se seleccione una encuesta
-elPeriodo_tipo.style.visibility="hidden";
-elPeriodo_anio.style.visibility="hidden";
-
 
 document.addEventListener('DOMContentLoaded', function(){
 	// Si el tipo de usuario es diferente de 2, se renderiza el menú y se cargan las encuestas disponibles
 	renderMenu();
 	cargarEncuestas();
 });
+
 
 // Función para cargar las encuestas disponibles desde el servidor
 function cargarEncuestas(){
@@ -58,198 +42,3 @@ function cargarEncuestas(){
 
 		})
 }
-
-// Se agrega un event listener al select de encuestas para cargar los periodos disponibles cuando se seleccione una encuesta
-elEncuestas.addEventListener('change', (event) => {
-	// Se limpian los selects de periodo y año cada vez que se selecciona una encuesta diferente
-	elPeriodo_tipo.innerHTML="<option value ='NINGUNO'>-- Seleccionar una periodo--</option>";
-	elPeriodo_anio.innerHTML="<option value ='NINGUNO'>-- Seleccionar una año--</option>";
-
-	// Si no se selecciona ninguna encuesta, se ocultan los filtros de periodo y año
-	if(elEncuestas.value === "NINGUNA"){
-		elPeriodo_tipo.style.visibility="hidden";
-		elPeriodo_anio.style.visibility="hidden";
-	}else{
-		// Si se selecciona una encuesta, se muestran los filtros de periodo 
-		// y año y se cargan los periodos disponibles para esa encuesta desde el servidor
-		elPeriodo_tipo.style.visibility="visible";
-		elPeriodo_anio.style.visibility="visible";
-
-
-		fetch('./obtener_periodos.php')
-			.then( res => res.json())
-			.then( function(respuesta){
-				console.log(respuesta);
-				if(respuesta.error){
-					// Si hay un error al obtener los periodos, se muestra un mensaje de error y se detiene la ejecución
-					lanzarToast(respuesta.error,"error");
-					return
-				}
-				// Por cada tipo de periodo obtenido, se crea una opción en el select de periodo tipo
-				respuesta.tipo.forEach(function(tipo){
-					const option = document.createElement('option');
-					// Se asigna el valor de la opción al tipo de periodo y el texto al tipo de periodo
-					option.value = tipo.Periodo_tipo;
-					option.textContent = tipo.Periodo_tipo;
-					// Se agrega la opción al select de periodo tipo
-					elPeriodo_tipo.appendChild(option);
-				});
-
-				// Por cada año obtenido, se crea una opción en el select de periodo año
-				respuesta.anio.forEach(function(anio){
-					const option = document.createElement('option');
-					// Se asigna el valor de la opción al año y el texto al año
-					option.value = anio.Periodo_anio;
-					option.textContent = anio.Periodo_anio;
-					// Se agrega la opción al select de periodo año
-					elPeriodo_anio.appendChild(option);
-				})
-			});
-
-	}
-});
-
-// Función para cargar los datos del reporte estadístico y generar la tabla de resultados
-function handleCargarDatos(){
-	// Se obtienen los valores seleccionados en los filtros de encuesta, periodo tipo y periodo año
-	if(elEncuestas.value === "NINGUNA"){
-		// Si no se ha seleccionado ninguna encuesta, se muestra un mensaje de error y se detiene la ejecución
-		lanzarToast("Debe seleccionar una encuesta para generar el reporte", "error");
-		return;
-	}
-	// Si no se ha seleccionado un periodo tipo o un periodo año, se muestra un mensaje de error y se detiene la ejecución
-	if(elPeriodo_tipo.value === "NINGUNO" || elPeriodo_anio.value === "NINGUNO"){
-		lanzarToast("El periodo y el año deben rellenarse","error")
-		return;
-	}
-	// Si se han seleccionado todos los filtros, se procede a cargar la tabla de resultados del reporte estadístico
-	fetchRenderTabla();
-}
-function fetchRenderTabla(){
-
-	// Se obtienen los elementos del DOM para los títulos de la tabla
-	const elTitulos = document.getElementById("Titulos");
-	// Se obtienen los elementos del DOM para la tabla de resultados
-	const elTabla = document.getElementById("Tabla");
-	// Se obtienen los botones para generar Excel y PDF
-	const elBtnExcel = document.getElementById("btnGenerarExcel");
-	const elBtnPDF = document.getElementById("btnGenerarPDF");
-	// Se obtienen los valores seleccionados en los filtros de encuesta
-	const id_encuesta = elEncuestas.value;
-	// Se obtienen los valores seleccionados en los filtros de periodo tipo
-	const periodo_tipo = elPeriodo_tipo.value;
-	// Se obtienen los valores seleccionados en los filtros de periodo año
-	const periodo_anio = elPeriodo_anio.value;
-
-	// Se limpian las tablas de caulquier contenido que tengan
-	elTitulos.innerHTML=""
-	elTabla.innerHTML="";
-
-	// Se realiza una petición al servidor para obtener los 
-	// datos del reporte estadístico en formato JSON, 
-	// enviando los filtros seleccionados como parámetros
-	fetch("./generar_tabla.php",{
-		method: "POST",
-		headers:{
-			"Content-Type":"application/json"
-		},
-		body: JSON.stringify({Id_encuesta: id_encuesta, Periodo_tipo: periodo_tipo, Periodo_anio: periodo_anio})
-	}) 
-		.then(function (respuesta){
-			return respuesta.json();
-		})
-		.then(function (impresion){
-			if(impresion.error){
-				// Si hay un error al obtener los datos del reporte, 
-				// se muestra un mensaje de error y se ocultan los botones de Excel y PDF
-				lanzarToast(impresion.error,"error");
-
-				elBtnPDF.style.visibility="hidden";
-				elBtnExcel.style.visibility="hidden";
-
-				return
-			}
-
-			//Si la impresión es de tamaño 0 significa que la respuesta es un mensaje
-			//por lo que no se encontraron resultados.
-			if(impresion.length==0){
-				// Si no se encontraron resultados para los filtros seleccionados,
-				// se muestra un mensaje de error y se ocultan los botones de Excel y PDF
-				lanzarToast("No se encontraron Resultados", "error");
-				elBtnExcel.style.visibility="hidden";
-				elBtnPDF.style.visibility="hidden";
-				return;
-			}
-
-			// Renderizado de los títulos de la tabla, se toma el primer elemento de la 
-			// impresión para obtener los títulos de las columnas
-			Object.keys(impresion[0]).forEach(function(titulo){
-				// Por cada título, se agrega una celda de encabezado a la fila de títulos de la tabla
-				elTitulos.innerHTML=elTitulos.innerHTML+"<th>"+titulo+"</th>"
-			});
-			// Renderizado de las filas de la tabla.
-			impresion.forEach(function(fila){
-				// Se crea una variable para almacenar la estructura HTML de la fila de la tabla
-				let table="";
-
-				// Se inicia la estructura de la fila con la etiqueta <tr>
-				table=table+"<tr>";
-				// Por cada dato en la fila, se agrega una celda a la estructura HTML de la fila
-				Object.keys(fila).forEach(function(dato){
-					// Se agrega una celda a la estructura HTML de la fila con el valor del dato correspondiente
-					table=table+"<td>"+fila[dato]+"</td>";
-
-				});
-				// Se cierra la estructura de la fila con la etiqueta </tr>
-				table=table+"</tr>";
-				// Se agrega la estructura HTML de la fila a la tabla de resultados en el DOM
-				elTabla.innerHTML+=table;
-			});
-
-			// Después de renderizar la tabla, se agrega una fila adicional para mostrar el promedio del reporte estadístico
-			elTabla.innerHTML+="<tr><td><h2>Promedio:</h2><label id='lblPromedio'></label></td><tr>"
-			// Se muestran los botones para generar Excel y PDF después de cargar la tabla de resultados
-			elBtnExcel.style.visibility="visible";
-			elBtnPDF.style.visibility="visible";
-			// Se carga el promedio del reporte estadístico después de renderizar la tabla de resultados
-			fetchPromedio();
-		});
-}
-
-
-// Función para cargar el promedio del reporte estadístico y mostrarlo en la tabla de resultados
-function fetchPromedio(){
-	// Se obtiene el elemento del DOM para mostrar el promedio
-	const promedio=document.getElementById("lblPromedio");
-	// Se obtienen los valores seleccionados en el filtro de encuesta
-	const id_encuesta = document.getElementById("slctEncuestas").value;
-	// Se obtienen los valores seleccionados en el filtro de periodo tipo
-	const periodo_tipo = document.getElementById("slctPeriodoTipo").value;
-	// Se obtienen los valores seleccionados en el filtro de periodo año
-	const periodo_anio = document.getElementById("slctPeriodoAnio").value;
-
-	// Se realiza una petición al servidor para obtener el promedio del reporte estadístico en formato JSON,
-	// enviando los filtros seleccionados como parámetros
-	fetch("./generar_promedio.php",{
-		method: "POST",
-		headers:{
-			"Content-Type":"application/json"
-		},
-		// Se envían los filtros seleccionados como parámetros en el cuerpo de la petición
-		body: JSON.stringify({Id_encuesta: id_encuesta, Periodo_tipo: periodo_tipo, Periodo_anio: periodo_anio})
-	}).then(function (respuesta){
-		return respuesta.json();
-	})
-		.then(function (impresion){
-			if(impresion.error){
-				// Si hay un error al obtener el promedio, se muestra un mensaje de error y se detiene la ejecución
-				lanzarToast(impresion.error,"error");
-				return
-			}
-			// Si se obtiene el promedio correctamente, se muestra el valor del promedio en el elemento del DOM correspondiente
-			promedio.innerHTML=impresion;		
-		})
-	
-	// Después de cargar el promedio, se muestra un mensaje de éxito indicando que los resultados se cargaron correctamente
-	lanzarToast("Resultados cargados correctamente", "exito");
-}	
