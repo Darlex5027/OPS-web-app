899b56062be4f283dade4797844ac55d08b677a9
danyhd24 <20232389@uatx.mx>
2026-05-07 15:08:12 -0600

    fix / correccion de redirecciones menu
    correccion de redirecciones del menu y cambio de normbre en los titulo

--- app/js/menu.js
+++ app/js/menu.js
@@ -14,15 +14,16 @@ export function renderMenu() {
     if (tipoUsuario == '1' || tipoUsuario == '3') {
         elMenu.innerHTML = `
             <a href="../CU_03_PerfilGestionable/perfil.html"><button>Perfil</button></a>
-            <a href="../CU_06_PublicarVacantes/admin_vacantes.html"><button>Publicar vacantes para servicio social</button></a>
-            <a href="../CU_07_AdministrarAlumnos/admin_alumnos.html"><button>Administrar alumnos</button></a>
-            <a href="../CU_08_Reporte_Alumnado/reporte_alumnos.html"><button>Generar reporte de alumnos</button></a>
-            <a href="admin_encuestas.html"><button>Editar formularios</button></a>
+            <a href="../CU_06_PublicarVacantes/admin_vacantes.html"><button>Publicar vacantes</button></a>
+            <a href="../CU_07_AdministrarAlumnos/admin_usuarios.html"><button>Administrar usuarios</button></a>
+            <a href="../CU_08_Reporte_Alumnado/reporte_alumnos.html"><button>sGenerar reporte de alumnos</button></a>
             <a href="../CU_09_ReporteEstadistico/reporte_estadistico.html"><button>Generar reporte estadistico</button></a>
+            <a href="../CU_12_CrearFormularios/encuestas_lista.html"><button>Ver formularios</button></a>
         `;
     } else if (tipoUsuario == '2') {
         elMenu.innerHTML = `
             <a href="admin_encuestas.html"><button>Responder formularios</button></a>
+            <a href="admin_encuestas.html"><button>Vacantes</button></a>
             <a href="../CU_03_PerfilGestionable/perfil.html"><button>Perfil</button></a>
         `;
     }
